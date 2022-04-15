1eb585337d9e0bd845cbb832d33ea5a6090854a2
chetanchatrath <45016862+chetanchatrath@users.noreply.github.com>
2022-04-15 12:46:42 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,8 +12,8 @@ provider "azurerm" {
   
   subscription_id = "c614835e-9b30-4377-8676-159532c4a446"
   tenant_id = "7a4d8448-3ab0-4c53-85f5-4f98b0e89d44"
-  client_id = "94871431-da9b-46b9-af7b-f6294cdeffa2"
-  client_secret = "SyM7Q~P2vEVy~nXNxu6-qSKb5ZVHZClXtTQcR"
+  client_id = "113aed03-3be9-4eb5-b55d-c01be05c4f76"
+  client_secret = "UnJ8Q~m5cZEQIJyHuw~FeLStZowqinXUJBGGnaqR"
 }
 
 resource "azurerm_resource_group" "myresourcegroup" {
