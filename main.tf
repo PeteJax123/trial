b71628b59a3cf5fb92998272adf8a0495d150ccf
chetanchatrath <45016862+chetanchatrath@users.noreply.github.com>
2022-04-15 12:42:05 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,19 @@
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "=2.60.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
 
 provider "azurerm" {
-  subscription_id = "var.TF_VAR_ARM_SUBSCRIPTION_ID"
-  tenant_id = "var.TF_VAR_ARM_TENANT_ID"
-  client_id = "var.TF_VAR_ARM_CLIENT_ID"
-  client_secret = "var.TF_VAR_ARM_CLIENT_SECRET"
+  features{}
+  
+  subscription_id = "c614835e-9b30-4377-8676-159532c4a446"
+  tenant_id = "7a4d8448-3ab0-4c53-85f5-4f98b0e89d44"
+  client_id = "94871431-da9b-46b9-af7b-f6294cdeffa2"
+  client_secret = "SyM7Q~P2vEVy~nXNxu6-qSKb5ZVHZClXtTQcR"
 }
 
 resource "azurerm_resource_group" "myresourcegroup" {
