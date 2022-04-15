7ca06a5c4415b0c3beb822ff8cf5190cb5c7a022
chetanchatrath <45016862+chetanchatrath@users.noreply.github.com>
2022-04-15 12:36:09 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,17 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.60.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "=2.60.0"
+#     }
+#   }
+# }
 
 provider "azurerm" {
-  features {}
+  subscription_id = "var.TF_VAR_ARM_SUBSCRIPTION_ID"
+  tenant_id = "var.TF_VAR_ARM_TENANT_ID"
+  client_id = "var.TF_VAR_ARM_CLIENT_ID"
+  client_secret = "var.TF_VAR_ARM_CLIENT_SECRET"
 }
 
 resource "azurerm_resource_group" "myresourcegroup" {
