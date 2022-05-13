c3886ced402b978d22fb39c8d0b23b638e3d7e1f
PeteJax123 <67374382+PeteJax123@users.noreply.github.com>
2022-05-13 15:10:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,19 +17,10 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "myresourcegroup" {
-  name     = "chetannew1"
+  name     = "chet789"
   location = "canadaeast"
 
   tags = {
     environment = "Production"
   }
-}
-
-
-# Create a virtual network within the resource group
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  address_space       = ["10.0.0.0/16"]
 }
