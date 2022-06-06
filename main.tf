1c01442891e69c474497f32a2e998d6e7506e24b
PeteJax123 <67374382+PeteJax123@users.noreply.github.com>
2022-06-06 15:28:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,10 +21,10 @@ terraform {
 provider "azurerm" {
   features{}
   
-  subscription_id = "2d383b10-41b4-4814-a1b5-2b3d9946dc1b"
-  tenant_id = "58a5f423-d654-4713-9407-dd52acaeee23"
-  client_id = "01026030-be41-4e99-9f34-9f939a14bf47"
-  client_secret = "u-R8Q~9PYaUOlH4EMvSKVISTfRvG41MoEVAKPajc"
+  subscription_id = ""
+  tenant_id = ""
+  client_id = ""
+  client_secret = ""
 }
 
 resource "azurerm_resource_group" "myresourcegroup" {
