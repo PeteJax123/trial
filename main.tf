2dae9e68ceaad759474ca35a7fb3aab30ba4cb4a
PeteJax123 <67374382+PeteJax123@users.noreply.github.com>
2022-06-07 16:24:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ terraform {
 provider "azurerm" {
   features{}
   
-  subscription_id = "2d383b10-41b4-4814-a1b5-2b3d9946dc1b"
-  tenant_id = "58a5f423-d654-4713-9407-dd52acaeee23"
-  client_id = "e1cef5ac-cedc-4598-96ef-1dd6f752b3fa"
-  client_secret = "vES8Q~92h6gFHmhuy74YDnFHPKnbQlcSFvCSvbLT"
+  subscription_id = ""
+  tenant_id = ""
+  client_id = ""
+  client_secret = ""
 }
 
 resource "azurerm_resource_group" "myresourcegroup" {
