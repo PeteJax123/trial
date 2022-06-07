11923cd229acee9ad4423f248a414bff424ab732
PeteJax123 <67374382+PeteJax123@users.noreply.github.com>
2022-06-07 15:21:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,28 +7,17 @@ terraform {
   }
 }
 
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "trial-test"
-
-    workspaces {
-      name = "june-06-WS-03"
-    }
-  }
-}
-
 provider "azurerm" {
   features{}
   
-  subscription_id = ""
-  tenant_id = ""
-  client_id = ""
-  client_secret = ""
+  subscription_id = "2d383b10-41b4-4814-a1b5-2b3d9946dc1b"
+  tenant_id = "58a5f423-d654-4713-9407-dd52acaeee23"
+  client_id = "e1cef5ac-cedc-4598-96ef-1dd6f752b3fa"
+  client_secret = "vES8Q~92h6gFHmhuy74YDnFHPKnbQlcSFvCSvbLT"
 }
 
 resource "azurerm_resource_group" "myresourcegroup" {
-  name     = "June-TFC-Org-TWO"
+  name     = "June-Seven-One"
   location = "canadaeast"
 
   tags = {
